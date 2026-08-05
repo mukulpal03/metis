76f35f44b0856e42a8172b46c03dc0d0fe8718d0
Mukul Pal <mukulpal0333@gmail.com>
2026-08-05 16:26:29 +0530

    feat: add google social provider

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,29 @@
-import "dotenv/config"
-import express from "express"
+import "dotenv/config";
+import express from "express";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 3000
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.all("/api/auth/*", toNodeHandler(auth));
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
 
 app.get("/health", (_, res) => {
-    return res.status(200).json({})
-})
+  return res.status(200).json({});
+});
 
 app.listen(PORT, () => {
-    console.log("Server is running on port", PORT)
-})
+  console.log("Server is running on port", PORT);
+});
--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -1,9 +1,19 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { deviceAuthorization } from "better-auth/plugins";
 import { db } from "./db";
 
 export const auth = betterAuth({
     database: prismaAdapter(db, {
         provider: "postgresql",
     }),
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        },
+    },
+    plugins: [
+        deviceAuthorization(),
+    ],
 });
